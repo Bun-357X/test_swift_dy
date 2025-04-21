27e9b436f43161bcb481cb80221d9a597e57d11b
LAPTOP-UBTJ1MS0\banhan <banbun.357@gmail.com>
2025-04-21 14:42:21 +0700

    add api url and swagger ui

--- 5_rest_api/exam_app/urls.py
+++ 5_rest_api/exam_app/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
 from .views import SchoolViewSet, ClassroomViewSet, TeacherViewSet, ClassroomTeacherViewSet, StudentViewSet
+
+# Swagger imports
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register('school', SchoolViewSet)
@@ -26,8 +31,25 @@ router.register('teacher', TeacherViewSet)
 router.register('classroom_teacher', ClassroomTeacherViewSet)
 router.register('student', StudentViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="API documentation with Swagger UI",
+      terms_of_service="^_^",
+      contact=openapi.Contact(email="you@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apis.urls')),
-    path('', include(router.urls)),
+    #path('api/', include('apis.urls')),
+    path('api/', include(router.urls)),
+
+    # Swagger URLs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
